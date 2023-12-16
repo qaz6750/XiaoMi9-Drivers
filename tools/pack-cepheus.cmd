9a751733fc8d486ab77c214a5dac424813b42ec9
ZeYan Li <qaz6750@outlook.com>
2023-12-16 11:57:47 +0800

    Tools: Add DriverUpdater for Offline/Online installation
    * Tools: Fix mistake making scripts only work on ARM64 based Windows devices
    * Tools: Update driverupdate to final version
    * Tools: Add Offline Updater
    
    Signed-off-by: ZeYan Li <qaz6750@outlook.com>

--- tools/pack-cepheus.cmd
+++ tools/pack-cepheus.cmd
@@ -2,6 +2,17 @@
 REM rmdir /Q /S ..\..\XiaoMi9-Drivers-Release
 mkdir ..\..\XiaoMi9-Drivers-Release
 
+echo @echo off > ..\OnlineUpdater.cmd
+echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\epsilon.txt >> ..\OnlineUpdater.cmd
+
+echo @echo off > ..\OfflineUpdater.cmd
+echo set /P DrivePath=Enter Drive letter ^^^(with the semi-column!^^^) of the connected device in mass storage mode ^^^(e.g. D:^^^): >> ..\OfflineUpdater.cmd
+echo DriverUpdater.%%PROCESSOR_ARCHITECTURE%%.exe -r . -d .\definitions\Desktop\ARM64\Internal\epsilon.txt -p %%DrivePath%% >> ..\OfflineUpdater.cmd
+
+copy DriverUpdater.ARM64.exe ..\
+copy DriverUpdater.AMD64.exe ..\
+copy DriverUpdater.X86.exe ..\
+
 echo apps\IPA >> filelist_cepheus.txt
 echo CODE_OF_CONDUCT.md >> filelist_cepheus.txt
 echo components\ANYSOC\Support\Desktop\SUPPORT.DESKTOP.BASE >> filelist_cepheus.txt
@@ -16,7 +27,12 @@ echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE >> filelist_cepheus.txt
 echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BASE_MINIMAL >> filelist_cepheus.txt
 echo components\QC8150\Platform\PLATFORM.SOC_QC8150.BATTERY >> filelist_cepheus.txt
 echo definitions\Desktop\ARM64\Internal\cepheus.txt >> filelist_cepheus.txt
+echo DriverUpdater.ARM64.exe >> filelist_cepheus.txt
+echo DriverUpdater.AMD64.exe >> filelist_cepheus.txt
+echo DriverUpdater.X86.exe >> filelist_cepheus.txt
 echo LICENSE.md >> filelist_cepheus.txt
+echo OfflineUpdater.cmd >> filelist_cepheus.txt
+echo OnlineUpdater.cmd >> filelist_cepheus.txt
 echo README.md >> filelist_cepheus.txt
 
 cd ..
@@ -24,4 +40,9 @@ cd ..
 "%ProgramFiles%\7-zip\7z.exe" a -t7z ..\XiaoMi9-Drivers-Release\XiaoMi9-Drivers-Desktop.7z @tools\filelist_cepheus.txt -scsWIN
 cd tools
 
+del ..\OfflineUpdater.cmd
+del ..\OnlineUpdater.cmd
+del ..\DriverUpdater.ARM64.exe
+del ..\DriverUpdater.AMD64.exe 
+del ..\DriverUpdater.X86.exe
 del filelist_cepheus.txt
